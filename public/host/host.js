f3efa6204c295ab4ffaebd4e029e831431a49d70
Thiago Silva <thiago25pro@gmail.com>
2021-02-03 19:08:54 -0300

    heroku link to call api

--- public/host/host.js
+++ public/host/host.js
@@ -1,1 +1,1 @@
-const baseURL = location.hostname.includes('localhost') ? 'http://localhost:3333/api' : ''
+const baseURL = location.hostname.includes('localhost') ? 'http://localhost:3333/api' : 'https://node-array-crud.herokuapp.com/api'
